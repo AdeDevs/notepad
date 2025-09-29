f44d48ea2c0669e3e0f3ffb0d7fe128b92ac5998
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-09-29 17:07:53 +0100

    added forgot password

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const username = document.getElementById("username")
 const email = document.getElementById("mail")
 const password = document.getElementById("password")
 const message = document.getElementById("msg")
+const forgot = document.getElementById("fgt")
 form.addEventListener("submit", preventDefault)
 
 function preventDefault(e) {
@@ -10,4 +11,12 @@ function preventDefault(e) {
         const value = username.value
         alert(`Hello ${value}`)
     console.log(password.value)
+
+    if (password.value != "password") {
+        alert("Wrong password")
+        forgot.style.display = "block"
+    } else if (password.value === "password") {
+        alert(`Welcome ${value}`)
+        forgot.style.display = "none"
+    }
 }
